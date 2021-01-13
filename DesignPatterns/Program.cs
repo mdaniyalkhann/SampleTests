37fb264324efaff5ea913fe3bc2fef65cddc6ac2
Muhammad Daniyal Khan <mdaniyal.khan@aurea.com>
2021-01-13 19:02:40 +0500

    Update Program.cs

--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -5,12 +5,7 @@
         static void Main(string[] args)
         {
             // Testing webhooks
-            // Testing webhook 1
-                        // Testing webhook 3
-            
-            
-            
-            
+            // Testing webhook
             
         }
     }
